02e7b74325062990ffafe4d7756bc983010608f2
or-abdillh <hans.abdillh05@gmail.com>
2022-12-29 01:17:42 +0800

    Install Html5qrcodeScanner

--- resources/views/pages/admin/scanner/index.blade.php
+++ resources/views/pages/admin/scanner/index.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
     <section class="container mb-5">
-        Barcode Scanner
+        <section class="row">
+            <section class="col-md-7 mx-auto">
+                <h4 class="text-center">Scanner</h4>
+                {{-- reader --}}
+                <section id="reader"></section>
+                {{-- form --}}
+                <form class="d-none" action="" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <input type="text" name="ticket_id" id="ticket_id">
+                </form>
+            </section>
+        </section>
     </section>
 @endsection
+
+@push('script')
+    @vite('resources/js/scanner/main.js')
+@endpush
