fdaceb4cc7acfbcd31607d708d903102a6769ea8
MRizkiAG <mrizkiag1027@gmail.com>
2022-12-28 15:56:59 +0800

    add some fields on user table & register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,9 +62,12 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'nik' => ['required', ' ', 'max:255'],
+            'phone' => ['required', 'string', 'max:255'],
+            'address' => ['required', 'string', 'max:255'],
         ]);
     }
-
+    
     /**
      * Create a new user instance after a valid registration.
      *
@@ -77,6 +80,9 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'nik' => $data['nik'],
+            'phone' => $data['phone'],
+            'address' => $data['address'],
         ]);
     }
 
