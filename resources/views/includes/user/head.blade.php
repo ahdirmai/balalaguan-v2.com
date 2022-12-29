79787c3531777e0df7ac0a5c4f208be2728eac9b
or-abdillh <hans.abdillh05@gmail.com>
2022-12-29 18:39:35 +0800

    Create base layout and page for landing page

--- resources/views/includes/user/head.blade.php
+++ resources/views/includes/user/head.blade.php
@@ -20,5 +20,23 @@
 <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('/icons/icon-96x96.png') }}">
 <link rel="icon" type="image/png" sizes="72x72" href="{{ asset('/icons/icon-72x72.png') }}">
 
+
 {{-- stylesheets --}}
+@vite([
+    'resources/css/brand/colors.css',
+    'resources/css/app.css'
+])
+
+{{-- Font awesome 6 --}}
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css" integrity="sha384-/frq1SRXYH/bSyou/HUp/hib7RVN1TawQYja658FEOodR/FQBKVqT9Ol+Oz3Olq5" crossorigin="anonymous"/>
+
+{{-- Poppins --}}
+<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+<style>
+    * {
+        font-family: 'Poppins', sans-serif;
+    }
+</style>
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
--- resources/views/layouts/user/base.blade.php
+++ resources/views/layouts/user/base.blade.php
@@ -1,14 +1,22 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        @stack('style')
-        <title>Balalaguan - Bumantara Event Organizer</title>
-    </head>
-    <body>
-    hiihih
-
+<head>
+    @include('includes.user.head')
+    @stack('style')
+    <title>Balalaguan - Bumantara Event Organizer</title>
+</head>
+<body>
+    @include('includes.user.navbar')
+    @yield('outer')
+    <main class="bg-light">
+        <section class="container-lg">
+            @yield('content')
+        </section>
+    </main>
+    @include('includes.user.footer')
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    @vite('resources/js/app.js')
 
     @stack('script')
 </body>
