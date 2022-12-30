0dfd2c18777ae39fd6363f27d596b999ca939bf3
or-abdillh <hans.abdillh05@gmail.com>
2022-12-31 03:20:24 +0800

    remove jquery cdn

--- resources/views/pages/user/ticket/index.blade.php
+++ resources/views/pages/user/ticket/index.blade.php
@@ -22,6 +22,5 @@
 @endsection
 
 @push('script')
-    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
     @vite('resources/js/pusher.js')
 @endpush
