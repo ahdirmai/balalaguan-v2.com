3279f686d89739473577b3a0b8dd4f9eec685b2c
or-abdillh <hans.abdillh05@gmail.com>
2023-01-01 15:09:03 +0800

    Update includes.admin.base

--- resources/views/includes/admin/head.blade.php
+++ resources/views/includes/admin/head.blade.php
@@ -21,12 +21,11 @@
 
 {{-- styles --}}
 @vite([
-    "resources/css/core-ui/vendors/simplebar.css",
-    "resources/css/core-ui/vendors/chartjs.css",
     "resources/css/core-ui/style.css",
     "resources/css/core-ui/example.css",
     "resources/css/brand/colors.css"
 ])
+
 <!-- We use those styles to show code examples, you should remove them in your application.-->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/themes/prism.css">
 
@@ -36,6 +35,9 @@
 {{-- Core UI modal --}}
 <link rel="canonical" href="https://coreui.io/docs/components/modal/">
 
+{{-- simple-lightbox css --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.11.0/simple-lightbox.css" integrity="sha512-HL1y3YvMbRp23VyjlaZDXCwtM5iJ6amoDyEcjpANI6qObX9pb8B7c31UJVyesMb3LKvmS8sr6eKOFMAEaHgf+g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 {{-- scripts --}}
 <script src="https://cdn.jsdelivr.net/npm/@coreui/coreui@4.2.0/dist/js/coreui.bundle.min.js" integrity="sha384-n0qOYeB4ohUPebL1M9qb/hfYkTp4lvnZM6U6phkRofqsMzK29IdkBJPegsyfj/r4" crossorigin="anonymous"></script>
 
@@ -43,6 +45,8 @@
 
 <script src="https://cdn.jsdelivr.net/npm/@coreui/coreui@4.2.0/dist/js/coreui.min.js" integrity="sha384-2hww80ziDjQXYpFulPf5tfdCCXLTxn70HdSwL9MfeEvpS0kjfHd1iaBRsLpnuaSC" crossorigin="anonymous"></script>
 
+{{-- simple-lightbox --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.11.0/simple-lightbox.min.js" integrity="sha512-6MY1+N+yqKr8NaWCd7SsFAavdwAnxKihOP2BI0MnU+k8khGU+op83TxJE7dYj0o2mmtfDbRs7U1jAmAeEOjUnw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <!-- Global site tag (gtag.js) - Google Analytics-->
 <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-118965717-3"></script>
