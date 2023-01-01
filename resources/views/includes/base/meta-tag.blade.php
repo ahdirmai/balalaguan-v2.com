3c7ca8a3cfa234b3649fcbe19473e8ad2d49071e
or-abdillh <hans.abdillh05@gmail.com>
2023-01-01 17:52:09 +0800

    Update wellcome page and period.index view

--- resources/views/includes/base/meta-tag.blade.php
+++ resources/views/includes/base/meta-tag.blade.php
@@ -5,14 +5,14 @@
 
 <!-- Open Graph / Facebook -->
 <meta property="og:type" content="website">
-<meta property="og:url" content="https://metatags.io/">
+<meta property="og:url" content="https://balalaguanmusicfest.com/">
 <meta property="og:title" content="Balalaguan Music Festival - Bumantara Event Organizer">
 <meta property="og:description" content="Temukan dan beli tiket Balalaguan Festival Music di website kami. Kami menyediakan berbagai pilihan  paket tiket. Dapatkan tiket Anda hari ini dan saksikan penampilan dari artis ternama favorit Anda">
 <meta property="og:image" content="https://drive.google.com/uc?id=1bsLx9IybuDtHUTVzEeJN5VEsgx5lkoa_&export=download">
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image">
-<meta property="twitter:url" content="https://metatags.io/">
+<meta property="twitter:url" content="https://balalaguanmusicfest.com/">
 <meta property="twitter:title" content="Balalaguan Music Festival - Bumantara Event Organizer">
 <meta property="twitter:description" content="Temukan dan beli tiket Balalaguan Festival Music di website kami. Kami menyediakan berbagai pilihan  paket tiket. Dapatkan tiket Anda hari ini dan saksikan penampilan dari artis ternama favorit Anda">
 <meta property="twitter:image" content="https://drive.google.com/uc?id=1bsLx9IybuDtHUTVzEeJN5VEsgx5lkoa_&export=download">
--- resources/views/pages/admin/periods/index.blade.php
+++ resources/views/pages/admin/periods/index.blade.php
@@ -70,7 +70,9 @@
 @push('script')
     <script>
         $(document).ready( function() {
-            $('#table').DataTable()
+            $('#table').DataTable({
+                scrollX: true
+            })
         })
     </script>
 @endpush
